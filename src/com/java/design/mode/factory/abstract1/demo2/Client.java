66ee8a37630cba3b3a6b549430402b4c7136690c
张远 <472918259@qq.com>
2019-06-28 13:45:23 +0800

    模板方法模式

--- src/com/java/design/mode/factory/abstract1/demo2/Client.java
+++ src/com/java/design/mode/factory/abstract1/demo2/Client.java
@@ -14,12 +14,16 @@ public class Client {
 		
 		//产生A1对象
 		AbstractProductA a1 =  creator1.createProductA();
+		a1.doSomething();
 		//产生A2对象
 		AbstractProductA a2 = creator2.createProductA();
+		a2.doSomething();
 		//产生B1对象
 		AbstractProductB b1 = creator1.createProductB();
+		b1.doSomething();
 		//产生B2对象
 		AbstractProductB b2 = creator2.createProductB();
+		b2.doSomething();
 		
 		/*
 		 * 然后在这里就可以为所欲为了...
